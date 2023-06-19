1a75e8814269b8816f8c5b96daef3dcd015bf746
Fikayo Jetawo <jetawof@gmail.com>
2023-06-19 23:07:12 +0100

    About Component for Desktop

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,8 +1,24 @@
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 function About() {
+    const right = <FontAwesomeIcon icon={faAngleDoubleRight} />
     return (
-        <div className='p-5'>
+        <div id="about" className='md:text-left py-10 md:py-20 '>
+            <div className='gap-10 w-11/12 mx-auto flex justify-around'>
+                <div className='w-full'>
+                    <p className='inline px-2 py-1 border-l-4 border-bordercolor'>About Us</p>
+                    <h1 className='text-3xl md:text-4xl font-rubik my-4 font-semibold'>Get to know us better</h1>
+                    <p className='font-krub text-lg lg:text-xl md:py-5'>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex aliquid inventore minus quam fuga sed nisi, nemo sint, enim aliquam pariatur doloremque eos eum provident modi repudiandae culpa minima voluptatem! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, iure, architecto deserunt veniam corrupti laborum laudantium quo vel dolorum numquam saepe, repudiandae totam! Veniam autem, distinctio a quis facilis expedita.
+                    </p>
+                    <button className='w-fit my-4 bg-bordercolor px-4 py-3 mx-3 rounded-xl hover:bg-[#121212] duration-300 hover:scale-110 font-semibold text-white'>Learn More &nbsp;{right}</button>
+                </div>
+                <div className='hidden w-1/2 md:flex flex-col max-h-min self-center'>
+                    <img src='https://images.pexels.com/photos/1410235/pexels-photo-1410235.jpeg?auto=compress&cs=tinysrgb&w=2000' alt="illustration" className='self-center h-96 object-cover w-full rounded-3xl' />
+                </div>
+            </div>
         </div>
     )
 }
--- client/components/Hero.jsx
+++ client/components/Hero.jsx
@@ -1,12 +1,15 @@
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 function Hero() {
+    const right = <FontAwesomeIcon icon={faAngleDoubleRight} />
     return (
-        <div className='lg:p-16 lg:pt-36 py-20 px-10 flex w-full bg-gradient-to-br text-white  from-[#651549] via-[#961b0d] to-[#ed8419]'>
-            <div className='lg:w-1/2 flex flex-col justify-center'>
-                <label className='bg-white w-fit border-[#ed8419] px-2 border-l-4 block text-[#651549] font-semibold'>Foods and Drinks</label>
+        <div className='lg:p-16 lg:pt-36 py-20 px-10 flex w-full bg-gradient-to-br text-white  from-[#651549] via-[#961b0d] to-bordercolor'>
+            <div className='lg:w-1/2 flex space-y-5 flex-col justify-center'>
+                <label className='bg-white w-fit border-bordercolor px-2 border-l-4 block text-[#651549] font-semibold'>Foods and Drinks</label>
                 <h1 className='text-4xl mds:text-5xl lg:text-7xl font-semibold lg:leading-[75px]'>Hundreds of flavors <br />under one roof</h1>
-                <button className='w-fit my-4 bg-[#ed8419] px-4 py-3 mx-3 rounded-2xl font-semibold'>Explore Menu</button>
+                <button className='w-fit my-4 hover:scale-110 duration-300 bg-gradient-to-r to-[#651549]  from-bordercolor p-4 mx-3 rounded-2xl font-extrabold'>Explore Menu &nbsp;{right}</button>
             </div>
             <div className='hidden lg:block w-1/2'>
                 <img
